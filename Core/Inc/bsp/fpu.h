1e49734a5fe2d6a7811ed5eb069bbec55b06c695
Chengyun Zhu <zhuchengyun.hf@outlook.com>
2026-07-22 22:11:29 +0800

    feat: enable FPU and untrack .setting

---
+++ Core/Inc/bsp/fpu.h
@@ -0,0 +1,16 @@
+#ifndef BSP_FPU_H
+#define BSP_FPU_H
+
+#include "stm32f4xx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void bsp_FPU_Enable(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* BSP_FPU_H */
---
+++ Core/Src/bsp/fpu.c
@@ -0,0 +1,12 @@
+#include "bsp/fpu.h"
+
+void bsp_FPU_Enable(void)
+{
+#if defined(__ARM_FP) && (__ARM_FP != 0)
+    /* Coprocessor Access Control Register (CPACR) */
+    /* Set CP10 and CP11 Full Access (bits 20..23 = 0b11, 0b11) */
+    SCB->CPACR |= ((0b11UL << 20) | (0b11UL << 22));
+    __DSB();
+    __ISB();
+#endif
+}
