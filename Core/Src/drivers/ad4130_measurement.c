574e426ac52b9ed4d32d731c1a2200177031c685
Chengyun Zhu <zhuchengyun.hf@outlook.com>
2026-08-10 08:57:18 +0800

    feat!: add autorange current level
    Add autorange current level function and applied to ADC2 Channel 1. This
    is insufficiently tested.
    
    Signed-off-by: Chengyun Zhu <zhuchengyun.hf@outlook.com>

--- Core/Src/drivers/ad4130_measurement.c
+++ Core/Src/drivers/ad4130_measurement.c
@@ -1,5 +1,16 @@
 #include "drivers/ad4130_measurement.h"
 
+#define AD4130_DATA_LOW           0x346DC6U  /* 2.0 mV */
+#define AD4130_DATA_HIGH          0xD1B717U  /* 8.0 mV */
+#define AD4130_DATA_100NA_RETURN  0x01F751U  /* 7.5 mV at 10 µA */
+                                             /* 75 µV at 100 nA */
+
+static HAL_StatusTypeDef AD4130_Get_Autorange_Level(
+		uint8_t current_level,
+		uint32_t data,
+		uint8_t *new_level
+);
+
 HAL_StatusTypeDef AD4130_Read_Channel_0_Resistance(
 		uint8_t adc_device_id,
 		float *resistance
@@ -12,6 +23,8 @@ HAL_StatusTypeDef AD4130_Read_Channel_0_Resistance(
 	uint8_t channel;
 	float iout;
 	float voltage;
+	uint8_t iout_level;
+	uint8_t new_iout_level;
 
 	if ((adc_device_id < 1U) || (adc_device_id > 2U) || (resistance == NULL))
 	{
@@ -55,5 +68,120 @@ HAL_StatusTypeDef AD4130_Read_Channel_0_Resistance(
 
 	*resistance = voltage / iout;
 
+	iout_level = ad4130_iouts[adc_device_id - 1U].level_1;
+	new_iout_level = iout_level;
+	result = AD4130_Get_Autorange_Level(iout_level, data, &new_iout_level);
+	if (result != HAL_OK)
+	{
+		return result;
+	}
+
+	if (new_iout_level != iout_level)
+	{
+		result = AD4130_Channel_0(adc_device_id, new_iout_level);
+		if (result != HAL_OK)
+		{
+			return result;
+		}
+	}
+
+	if (new_iout_level == 0U)
+	{
+		return HAL_ERROR;
+	}
+
+	return HAL_OK;
+}
+
+static HAL_StatusTypeDef AD4130_Get_Autorange_Level(
+		uint8_t current_level,
+		uint32_t data,
+		uint8_t *new_level
+)
+{
+	if ((current_level < 1U) || (current_level > 7U) || (new_level == NULL))
+	{
+		return HAL_ERROR;
+	}
+
+	data &= 0xFFFFFFU;
+	*new_level = current_level;
+
+	/*
+	 * 100 nA is a special level.
+	 */
+	if (current_level == 1U)
+	{
+		if (data <= AD4130_DATA_100NA_RETURN)
+		{
+			*new_level = 2U;  /* 100 nA -> 10 µA */
+		}
+		return HAL_OK;
+	}
+
+	if (data >= AD4130_DATA_HIGH)
+	{
+		switch (current_level)
+		{
+			case 2U:
+				*new_level = 1U;  /* 10 µA -> 100 nA */
+				break;
+
+			case 3U:
+				*new_level = 2U;  /* 20 µA -> 10 µA */
+				break;
+
+			case 4U:
+				*new_level = 3U;  /* 50 µA -> 20 µA */
+				break;
+
+			case 5U:
+				*new_level = 4U;  /* 100 µA -> 50 µA */
+				break;
+
+			case 6U:
+				*new_level = 5U;  /* 150 µA -> 100 µA */
+				break;
+
+			case 7U:
+				*new_level = 5U;  /* 200 µA -> 100 µA */
+				break;
+
+			default:
+				return HAL_ERROR;
+		}
+	}
+	else if (data <= AD4130_DATA_LOW)
+	{
+		switch (current_level)
+		{
+			case 2U:
+				*new_level = 3U;  /* 10 µA -> 20 µA */
+				break;
+
+			case 3U:
+				*new_level = 4U;  /* 20 µA -> 50 µA */
+				break;
+
+			case 4U:
+				*new_level = 5U;  /* 50 µA -> 100 µA */
+				break;
+
+			case 5U:
+				*new_level = 7U;  /* 100 µA -> 200 µA */
+				break;
+
+			case 6U:
+				*new_level = 7U;  /* 150 µA -> 200 µA */
+				break;
+
+			case 7U:
+				*new_level = 0U;  /* 200 µA -> 0 µA */
+				break;
+
+			default:
+				return HAL_ERROR;
+		}
+	}
 	return HAL_OK;
 }
