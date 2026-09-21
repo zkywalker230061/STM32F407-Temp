5e1bb413a28c41322d7e86be75d2fc28f6d2cba3
Chengyun Zhu <zhuchengyun.hf@outlook.com>
2026-09-21 11:29:20 +0800

    feat: change MX init order
    Signed-off-by: Chengyun Zhu <zhuchengyun.hf@outlook.com>

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -106,10 +106,10 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_SPI3_Init();
   MX_USB_DEVICE_Init();
   MX_RTC_Init();
   MX_TIM7_Init();
-  MX_SPI3_Init();
   /* USER CODE BEGIN 2 */
 
 	int adc_result;
